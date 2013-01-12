6afa7d3ef6fc0275c55da441b4bb526ae833165c
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2013-01-12 04:01:07 -0500

    finally finished auth.js

--- Chrome Extension/auth.js
+++ Chrome Extension/auth.js
@@ -1,56 +1,35 @@
-function testAPI() {
-  alert('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
-    alert('Good to see you, ' + response.name + '.');
-  });
-}
+// I am so proud of this. --- Shane
 
-function login() {
-    FB.login(function(response) {
-        if (response.authResponse) {
-            // connected
-            testAPI();
-        } else {
-            // cancelled
-        }
+var user_data = "";
+
+function auth() {
+  // client_id is our application ID
+  var validate_url = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=478063252251631&redirect_uri=https://obscure-reaches-7009.herokuapp.com/&scope=read_stream,offline_access";
+  // to check that it's loaded
+  var isLoaded = false;
+  var accessToken = "";
+  var token_url = "";
+
+  if (!isLoaded) {
+    window.open(validate_url);  
+  }
+
+  // listens for the right pageload
+  chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+      token_url = changeInfo.url;
+      // 1) the page hasn't been loaded previously
+      // 2) the URL matches (er, close enough)
+      if (!isLoaded && token_url.substr(0,14) === "https://obscur") {
+        isLoaded = true;
+
+        accessToken = token_url.substring(token_url.indexOf("#access_token") + 14, token_url.indexOf("&"));
+        
+        chrome.tabs.remove(tabId);
+
+        user_data = "https://graph.facebook.com/me?fields=friends.fields(name,username)&access_token=" + accessToken;
+      }
     });
 }
 
-window.fbAsyncInit = function() {
-  console.log("init started");
-  // init the FB JS SDK
-  FB.init({
-    appId      : '478063252251631', // App ID from the App Dashboard
-    status     : true, // check the login status upon init?
-    cookie     : true, // set sessions cookies to allow your server to access the session?
-    xfbml      : true  // parse XFBML tags on this page?
-  });
-  console.log("init complete");
-
-  FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      // connected
-    } else if (response.status === 'not_authorized') {
-      // not_authorized
-      login();
-    } else {
-      // not_logged_in
-      login();
-    }
-  });
-
-};
-
-// Load the SDK's source Asynchronously
-// Note that the debug version is being actively developed and might 
-// contain some type checks that are overly strict. 
-// Please report such bugs using the bugs tool.
-console.log("test 1");
-(function(d, debug){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "https://connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
-   ref.parentNode.insertBefore(js, ref);
- }(document, /*debug*/ false));
-console.log("test 2");
+auth();
