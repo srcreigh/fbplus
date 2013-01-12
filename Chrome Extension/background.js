6d421642c469a87dbf54d0d7cf320313a900a2ff
Liam Horne <lihorne@uwaterloo.ca>
2013-01-11 20:10:23 -0500

    Added functionality
    fb home now works

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,18 +1,30 @@
-
-// This event is fired each time the user updates the text in the omnibox,
-// as long as the extension's keyword mode is still active.
+/* This is for WHILE the user is typing in his/her command
+   so we can add suggestions  */
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     console.log('inputChanged: ' + text);
     suggest([
       {content: text + " one", description: "the first one"},
       {content: text + " number two", description: "the second entry"}
-    ]);
+      ]);
   });
 
-// This event is fired with the user accepts the input in the omnibox.
+/* This is for after they press ENTER. 'text' is the string they send */
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    console.log('inputEntered: ' + text);
-    alert('You just typed "' + text + '"');
-  });
+
+  var cmd; // variable that will become the first command. e.g., "msg Liam" => cmd = msg
+
+  if (text.search(" ") == -1) { // If there is no space character in the string they send, this returns -1
+       cmd = text;                         // so we assume the whole string is a command
+  } else {
+    cmd = text.substr(0, text.indexOf(" ")); // otherwise cmd is the first word up to a space character
+  }
+
+  if (cmd == "home") {
+    chrome.tabs.getSelected( undefined, function(tab) {
+        chrome.tabs.update(tab.id, {url: "http://www.facebook.com/"}, undefined);
+        window.close(); 
+      });}
+
+});
