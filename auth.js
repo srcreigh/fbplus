f7c1172a1968b6fd56aa4ed16bf8a2f94719f5b0
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2013-01-14 18:18:34 -0500

    cleaned up auth.js, added some documentation

--- auth.js
+++ auth.js
@@ -1,30 +1,43 @@
-// I am so proud of this. --- Shane
+/* auth.js
+ * author: Shane R. Creighton-Young
+ * project: fbplus
+ *
+ */
 
-// local storage
+// External check for init
 if (localStorage['init'] === undefined) {
   auth();
 }
 
+/*
+ * auth(): Void -> Void
+ *         PRE: localStorage['init'] is undefined
+ *         POST: localStorage['init'] contains a JSON object with the user's friends' data
+ *
+ * This function uses Facebook's URL authorization system to retrieve an access token and query
+ * the Facebook Graph API to get data.
+ *
+ */
 
 function auth() {
-  // client_id is our application ID
+  // This URL requires out application ID, our redirection URL, and other options. 
+  // NOTE: if you need to query with extra privacy settings, edit the part at the end with the
+  // "scope" parameter.
   var validate_url = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=478063252251631&redirect_uri=https://obscure-reaches-7009.herokuapp.com/&scope=read_stream,offline_access";
-  // to check that it's loaded
+  
   var accessToken = "";
   var token_url = "";
 
   window.open(validate_url);
 
-  // listens for the right pageload
   chrome.tabs.onUpdated.addListener(
     function handler(tabId, changeInfo, tab) {
       token_url = changeInfo.url;
-      // 1) the page hasn't been loaded previously
-      // 2) the URL matches (er, close enough)
       if (token_url.substr(0,14) === "https://obscur") {
         // removes the event listener
         chrome.tabs.onUpdated.removeListener(handler);
 
+        // This extracts the access token from the new URL
         accessToken = token_url.substring(token_url.indexOf("#access_token") + 14, token_url.indexOf("&"));
         
         // closes the access token webpage
@@ -33,7 +46,7 @@ function auth() {
         // location of the Facebook Graph API request
         var data_url = "https://graph.facebook.com/me?fields=friends.fields(username,name,gender,hometown,birthday)&access_token=" + accessToken;
 
-        // gets the json file
+        // Queries the Facebook Graph API and stores it in localStorage
         $.ajax({
           url: data_url,
           jsonp: 'callback',
