7013e01230886a7ed5aff58e564d2aaa1ae8aa95
srcreigh <srcreigh@uwaterloo.ca>
2013-01-21 19:44:55 -0500

    added header to suggestions.js

--- suggestions.js
+++ suggestions.js
@@ -1,28 +1,8 @@
-
-
-/*obj.sort(function(a,b){
-	if (a.name < b.name)
-		return -1;
-	if (a.name > b.name)
-		return 1;
-	return 0;
-})
-
-var list_players = []
-
-chrome.omnibox.onInputChanged.addListener(
-  function(text, suggest) {
-  	list_players = obj.filter(function(elem){
-  		return (text === elem.substr(0,text.length));
-  	})
-  	list_players = list_players.slice(0.5);
-    console.log('inputChanged: ' + text);
-    suggest([{content: 'hi', description: 'you ;)'}]);
-  });*/
-
-// This is copied straight from the example.
-// This event is fired each time the user updates the text in the omnibox,
-// as long as the extension's keyword mode is still active.
+/* background.js
+ * author: Shane Creighton-Young
+ * project: fbplus
+ *
+ */
 
 var commands = new Array("apps", "events", "friends", "gender", "groups", "home", "id", "msg", "pics", "username");
 
